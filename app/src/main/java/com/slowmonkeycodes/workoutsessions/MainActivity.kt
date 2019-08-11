b26cae63d78de03abcf3fba59c625fc7f39d1e51
Gavin Chin <slowmonkeyking+github@gmail.com>
2019-08-11 19:35:22 +0100

    Adding pause and stop buttons

--- app/src/main/java/com/slowmonkeycodes/workoutsessions/MainActivity.kt
+++ app/src/main/java/com/slowmonkeycodes/workoutsessions/MainActivity.kt
@@ -2,11 +2,37 @@ package com.slowmonkeycodes.workoutsessions
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    enum class TimerState {
+        Stopped, Paused, Running
+    }
+
+    private lateinit var sessionButton: Button
+    private lateinit var sessionTimer: Timer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        resetWorkout()
+
+        sessionButton.setOnClickListener {
+            view ->
+            startSessionTimer()
+        }
+    }
+
+    private fun resetWorkout() {
+        sessionTimer = object: Timer() {
+            over
+        }
+    }
+
+    private fun startSessionTimer() {
+
     }
 }
