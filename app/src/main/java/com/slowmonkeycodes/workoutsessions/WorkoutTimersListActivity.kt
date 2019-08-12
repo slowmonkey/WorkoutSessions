dc9d22c0fe15f399fdd508eb91a5a4a68e827c54
Gavin Chin <slowmonkeyking+github@gmail.com>
2019-08-13 00:29:20 +0100

    Add cancel button action to return to previous activity

--- app/src/main/java/com/slowmonkeycodes/workoutsessions/WorkoutTimersListActivity.kt
+++ app/src/main/java/com/slowmonkeycodes/workoutsessions/WorkoutTimersListActivity.kt
@@ -2,11 +2,20 @@ package com.slowmonkeycodes.workoutsessions
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class WorkoutTimersListActivity : AppCompatActivity() {
+
+    private lateinit var cancelAddWorkoutButton : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_workout_timers_list)
+
+        cancelAddWorkoutButton = findViewById(R.id.cancel_add_workout_button)
+
+        cancelAddWorkoutButton.setOnClickListener {
+            finish()
+        }
     }
 }
